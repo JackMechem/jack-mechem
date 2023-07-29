f99a360ff14b62a5d641fafc3b56b768f3527de6
JackMechem <mechemjack@gmail.com>
2023-07-29 08:06:12 -0700

    Cool Github Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <main className="absolute h-screen w-screen bg-black">
             <div className="absolute max-w-md min-w-min px-10 py-12 bg-gray-100 border-neutral-700 bg-neutral-800/30 border left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg opacity-90 bg-opacity-30 items-center flex flex-col shadow-[0_2px_15px_rgba(255,255,255,0.3)]">
                 <h1 className="text-yellow-200 mb-6 text-2xl font-semibold text-center">
-                    Website Under Construction (Test Build)
+                    Website Under Construction
                 </h1>
                 <Image
                     src={constructionEmoji.src}
@@ -26,13 +26,14 @@ export default function Home() {
                     mechemjack@gmail.com
                 </a>
                 <p className="text-slate-400">Website Developer and Designer</p>
-                <a
-                    className="text-gray-200 text-4xl text-center mt-8 cursor-pointer hover:drop-shadow-lg shadow-slate-100"
-                    href="https://github.com/JackMechem/jack-mechem"
-                >
-                    <BsGithub className="hover:drop-shadow-[0_0_5px_rgba(255,255,255,0.5)]" />
-                </a>
             </div>
+            <a
+                href="https://github.com/JackMechem/jack-mechem"
+                className="absolute flex left-1/2 -translate-x-1/2 top-8  items-center w-64 justify-evenly bg-gray-700 p-4 rounded-full border border-gray-600 shadow shadow-slate-600 cursor-pointer"
+            >
+                <BsGithub className="hover:drop-shadow-[0_0_5px_rgba(255,255,255,0.5)] text-2xl text-slate-300" />
+                <p className="text-slate-300">Contribute on Github</p>
+            </a>
         </main>
     );
 }
