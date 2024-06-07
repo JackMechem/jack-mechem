41e2ab9d865c54e552da345f53f7f4d24bc9d0af
Jack Mechem <mechemjack@gmail.com>
2024-06-07 09:15:59 -0700

    ✨ Added Vercel Speed Insights

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import Nav from "./components/nav";
 import LayoutWrapper from "./components/layoutWrapper";
 import Header from "./components/header";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: {
@@ -32,6 +33,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <SpeedInsights />
       <LayoutWrapper>
         <div className="fixed md:top-[20px] md:right-[20px] md:left-[20px] md:bottom-[20px] top-[5px] right-[5px] bottom-[5px] left-[5px] pt-[104px] bg-primary text-foreground overflow-y-scroll">
           <Header />
