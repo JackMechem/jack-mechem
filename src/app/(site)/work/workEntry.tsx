3063388de091c8b0775a2ebbffef6c101e5541cb
JackMechem <mechemjack@gmail.com>
2024-07-08 08:28:54 -0700

    🐛 Style Fixes
     - Added 20px margin to all no border images
     - Use box shadow instead of drop shadow because of bugs on safari

--- src/app/(site)/work/workEntry.tsx
+++ src/app/(site)/work/workEntry.tsx
@@ -7,37 +7,37 @@ import useComponentVisible from "../components/useComponentVisible";
 import FloatingWorkWindow from "../components/floatingWorkWindow";
 
 interface WorkEntryProps {
-  work: any;
+	work: any;
 }
 
 const WorkEntry = ({ work }: WorkEntryProps) => {
-  const sidebarState = useComponentVisible(false);
+	const sidebarState = useComponentVisible(false);
 
-  return (
-    <Container key={work.sys.id} className="flex flex-col">
-      <FloatingWorkWindow
-        work={work}
-        sideBarState={sidebarState}
-        className={sidebarState.isComponentVisible ? "fixed" : "hidden"}
-      />
-      <img
-        src={work.photo.url}
-        alt={"work photo"}
-        className="md:h-[400px] h-[300px] w-fit rounded-[30px] mb-[10px] object-cover hover:drop-shadow-bluexlr hover:scale-[1.01] hover:cursor-pointer"
-        onClick={() => {
-          sidebarState.setIsComponentVisible(true);
-        }}
-      />
-      <a href={work.link}>
-        <h3 className="text-blue flex items-center gap-3">
-          {work.title} <IconLink />
-        </h3>
-      </a>
-      <p className="font-semibold text-foreground-sec">
-        {work.shortDescription}
-      </p>
-    </Container>
-  );
+	return (
+		<Container key={work.sys.id} className="flex flex-col">
+			<FloatingWorkWindow
+				work={work}
+				sideBarState={sidebarState}
+				className={sidebarState.isComponentVisible ? "fixed" : "hidden"}
+			/>
+			<img
+				src={work.photo.url}
+				alt={"work photo"}
+				className="md:h-[400px] h-[300px] w-fit rounded-[30px] mb-[10px] object-cover hover:shadow-bluexlr hover:scale-[1.01] hover:cursor-pointer"
+				onClick={() => {
+					sidebarState.setIsComponentVisible(true);
+				}}
+			/>
+			<a href={work.link}>
+				<h3 className="text-blue flex items-center gap-3">
+					{work.title} <IconLink />
+				</h3>
+			</a>
+			<p className="font-semibold text-foreground-sec">
+				{work.shortDescription}
+			</p>
+		</Container>
+	);
 };
 
 export default WorkEntry;
